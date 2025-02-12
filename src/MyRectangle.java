c6f9f2ba08f1ff2a623efd3b661e424ed44b59b8
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-02-12 00:11:19 -0330

    Added MyRectangle.java

---
+++ src/MyRectangle.java
@@ -0,0 +1,50 @@
+public class MyRectangle {
+    private MyPoint topLeft;
+    private MyPoint bottomRight;
+
+    public MyRectangle(int x1, int y1, int x2, int y2) {
+        this.topLeft = new MyPoint(x1, y1);
+        this.bottomRight = new MyPoint(x2, y2);
+    }
+
+    public MyRectangle(MyPoint topLeft, MyPoint bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+    }
+
+    public MyPoint getTopLeft() {
+        return topLeft;
+    }
+
+    public MyPoint getBottomRight() {
+        return bottomRight;
+    }
+
+    public void setTopLeft(MyPoint topLeft) {
+        this.topLeft = topLeft;
+    }
+
+    public void setBottomRight(MyPoint bottomRight) {
+        this.bottomRight = bottomRight;
+    }
+
+    public int getWidth() {
+        return Math.abs(bottomRight.getX() - topLeft.getX());
+    }
+
+    public int getHeight() {
+        return Math.abs(bottomRight.getY() - topLeft.getY());
+    }
+
+    public int getArea() {
+        return getWidth() * getHeight();
+    }
+
+    public int getPerimeter() {
+        return 2 * (getWidth() + getHeight());
+    }
+
+    public String toString() {
+        return "Rectangle [Top Left: " + topLeft + ", Bottom Right: " + bottomRight + "]";
+    }
+}
